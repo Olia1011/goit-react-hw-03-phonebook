2251f8ad9f7faeb94537109cf87015a2592eeb5a
Olia1011 <olgamelnyk10111989@gmail.com>
2020-05-07 13:05:39 +0300

    Updates

---
+++ precache-manifest.ed30c7ee7e76513f6d8973a5d3d6a687.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d642731f94691a6b1f5454f8f51e4405",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "95a8dd3344cb81edbcaf",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.91690294.chunk.css"
+  },
+  {
+    "revision": "feccae9b20750091f244",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.8f6ef1d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.8f6ef1d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a8dd3344cb81edbcaf",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.d9203cb3.chunk.js"
+  },
+  {
+    "revision": "653c99886214260df0f5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.067fb2b4.js"
+  },
+  {
+    "revision": "014c7f030ef0c10e1cbdd3b3e6f277ae",
+    "url": "/goit-react-hw-03-phonebook/static/media/dancing-script-v14-latin-regular.014c7f03.woff"
+  },
+  {
+    "revision": "5c1198f377ebf0fe64dd1a42476f0309",
+    "url": "/goit-react-hw-03-phonebook/static/media/dancing-script-v14-latin-regular.5c1198f3.woff2"
+  }
+]);
